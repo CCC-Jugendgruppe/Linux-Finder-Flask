2e0bd7954d97202d7250285d10d4cbb82811c5e4
konradrundfunk <konrad@eichsfeld.net>
2020-06-29 16:22:46 +0200

    fully functional message presenter

--- virtualenv/app.py
+++ virtualenv/app.py
@@ -1,17 +1,62 @@
 from flask import Flask, request, render_template, url_for
+import json 
 
+#import everything from the param.json 
+json_file = open("param.json", "r", encoding="utf-8")
+param = json.load(json_file)
+json_file.close()
+
+#assing the values to work with them 
+questions = param["questions"]
+answers = param["answers"]
+
+#Flask startup routine
 app = Flask(__name__)
 
 
-@app.route("/test")
+#First the questions
+@app.route("/test/1")
 def index():
-    questions = ["Wie viel Ram hat dein Pc? ", "Wie viel Erfahrung hast du bereits"]
-    i = 0
-    for i in range(len(questions)):
-        question = {"question" : questions[i]}
-        answ1 = {"answ1" : "10GB"}
-        return render_template('dropdown.html', question = question, answ1 = answ1)
+    answ_1 = answers["1"]
+    num_answ = answ_1[-1]
+    quest = {"question" : questions[0]}
+    type_answ = answ_1[-2] 
+    if type_answ == "radio":
+        return render_template('dropdown.html', question = quest, answ1 = answ_1, num_answ = num_answ)
+    else:
+        return "else "
 
+@app.route("/test/2")
+def index_1():
+    answ_1 = answers["2"]
+    num_answ = answ_1[-1]
+    quest = {"question" : questions[1]}
+    type_answ = answ_1[-2] 
+    if type_answ == "radio":
+        return render_template('dropdown.html', question = quest, answ1 = answ_1, num_answ = num_answ)
+    else:
+        return "else "
+@app.route("/test/3")
+def index_2():
+    answ_1 = answers["3"]
+    num_answ = answ_1[-1]
+    quest = {"question" : questions[2]}
+    type_answ = answ_1[-2] 
+    if type_answ == "radio":
+        return render_template('dropdown.html', question = quest, answ1 = answ_1, num_answ = num_answ)
+    else:
+        return "else "
+
+
+@app.route("/input/<inp>", methods=["GET", "POST"])
+def inputfun(inp):
+    dictionarry = {'Test': "Mein name", "noch ein Name":"mein Name", "True": None}
+
+    return dictionarry #json.dumps(inp)
+
+@app.route("/json")
+def jsonfun(dictionarry):
+    return json.dumps(dictionarry)
 
 
 
