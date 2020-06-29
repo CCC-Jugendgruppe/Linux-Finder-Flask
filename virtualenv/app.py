fe9eb271892033998771554d37a7772c150327e7
konradrundfunk <konrad@eichsfeld.net>
2020-06-29 18:59:39 +0200

    first version ready

--- virtualenv/app.py
+++ virtualenv/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, render_template, url_for, make_response, redirect
 import json 
 
+#Config 
+
+global maxquestion 
+maxquestion = 1
+
+
+
+
 #import everything from the param.json 
 json_file = open("param.json", "r", encoding="utf-8")
 param = json.load(json_file)
@@ -13,14 +21,14 @@ answers = param["answers"]
 #Flask startup routine
 app = Flask(__name__)
 
+#for the progress of the user 
 global prog 
 
 #First the questions
-@app.route("/")
-def home():
-    return "Tesst"
+
 
 @app.route("/test/0")
+# the first Question 
 def index():
     answ_1 = answers["1"]
     prog = {"prog" : 1}
@@ -48,24 +56,26 @@ def index_2():
 @app.route("/input/<int:prog>/", methods=["GET", "POST"])
 def inputfun(prog):
     answer = ""
+    if prog > maxquestion:
+        prog = 0 
+        return redirect("/") 
     if request == 'POST':
         answer = request.form["answer"]
     else:
         answer = request.args.get("answer")
-    if prog > 1:
-        return redirect("/") 
     domain = "/test/{}".format(prog) 
     resp = make_response(redirect(domain))
     resp.set_cookie(str(prog), answer)
     return resp 
 
-
-@app.route("/json")
-def jsonfun(dictionarry):
-    return json.dumps(dictionarry)
-
-
-
+@app.route("/")
+#the final Webpage for a result 
+def home():
+    cookie = request.cookies.get("1")
+    cookie1 = request.cookies.get("2")
+    cookie2 = request.cookies.get("3")
+    return render_template("final.html",cookie = cookie, cookie1= cookie1 )
+    
 #Note in production please use debug False
 if __name__ == "__main__":
     app.run(debug=True)
